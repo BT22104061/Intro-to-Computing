ebcfd6950f5a51202638d1073bd7a23cca6fbebf
BT22104061 <121023570+BT22104061@users.noreply.github.com>
2022-12-19 15:35:45 +0530

    Delete tax.py

--- tax.py
+++
@@ -1,8 +0,0 @@
-gross_income=float(input("Enter gross income"))
-dependents=int(input("Enter number of dependents"))
-standard_ded=10000
-dependent_ded=3000
-tax_rate=0.2
-taxable_income=gross_income-standard_ded-(dependent_ded*dependents)
-tax=taxable_income*tax_rate
-print("Tax=",tax)
